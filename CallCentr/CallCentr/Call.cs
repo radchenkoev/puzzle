2d348cf07e7b06ecccf1d7c7e5b147dce965db51
radchenkohv <helena.radchenko@gmail.com>
2015-04-16 22:11:49 +0300

    CallCentr task

---
+++ CallCentr/CallCentr/Call.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCentr
+{
+    public class Call
+    {
+        public int Id { get; set; }
+        public DateTime BeginCall { get; set; }
+        public DateTime EndCall { get; set; }
+    }
+}
